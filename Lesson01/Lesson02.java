4249219c47102ab810b38583d74e4d7bc5d9e6d0
rrenat358 <rrenat358@gmail.com>
2022-04-12 13:27:35 +0300

    1327

--- Lesson01/Lesson02.java
+++ Lesson01/Lesson02.java
@@ -1,4 +1,33 @@
 package Lesson01;
 
 public class Lesson02 {
+    public static void main(String[] args) {
+        String[] arrayString = new String[5];
+        arrayString[0] = "100";
+        arrayString[1] = "200";
+        arrayString[2] = "300";
+        arrayString[3] = "400";
+        arrayString[4] = "500";
+
+        arrConvertToInt(arrayString);
+
+    }
+
+    public static void arrConvertToInt(String[] arr) {
+        String a = "";
+        for (int i = 0; i < arr.length; i++) {
+
+            a = arr[i];
+            int b = Integer.parseInt(a.trim());
+//            arr[i] = Integer.parseInt(arr[i]);
+//            arr[i] = Integer.parseInt(arr[i].trim());
+//            arr[i] = String.valueOf(Integer.parseInt(a));
+            System.out.println(b);
+
+        }
+
+
+    }
+
+
 }
