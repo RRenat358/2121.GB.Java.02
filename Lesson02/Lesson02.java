8e8acb4aa73a9c864a210b73231bd71ebb87c9cd
rrenat358 <rrenat358@gmail.com>
2022-04-14 18:46:00 +0300

    relase.20220414-1845

--- Lesson02/Lesson02.java
+++ Lesson02/Lesson02.java
@@ -2,21 +2,24 @@ package Lesson02;
 
 public class Lesson02 {
 
-    public static final int ARRAY_SIZE = 5;
+    public static final int ARRAY_SIZE = 4;
 
     public static void main(String[] args) throws MyArraySizeException {
 
         System.out.println("––––––––––––––––––––––––––––––");
         String[][] arrayString = new String[ARRAY_SIZE][ARRAY_SIZE];
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            for (int j = 0; j < ARRAY_SIZE; j++) {
+//        String[][] arrayString = new String[33][ARRAY_SIZE]; //для тестирования вызова ошибки
+//        String[][] arrayString = new String[ARRAY_SIZE][33]; //для тестирования вызова ошибки
+//        String[][] arrayString = new String[33][33]; //для тестирования вызова ошибки
+        for (int i = 0; i < arrayString.length; i++) {
+            for (int j = 0; j < arrayString[i].length; j++) {
                 arrayString[i][j] = "1";
             }
         }
         arrayString[1][1] = "1000000";
         arrayString[2][2] = "+200000";
-        arrayString[3][3] = "30000";
-//        arrayString[3][3] = "30000hhhhhhhhhhhhhh";
+//        arrayString[3][3] = "30000";
+        arrayString[3][3] = "30000%"; //для тестирования вызова ошибки
 
         for (int i = 0; i < arrayString.length; i++) {
             for (int j = 0; j < arrayString[i].length; j++) {
@@ -34,33 +37,29 @@ public class Lesson02 {
     }
 
     public static int arraySum(String[][] arr) {
-        checkArraySize(arr);
-
+        checkArraySize("Строки", arr.length);
         int arrElement = 0;
         int arrSum = 0;
         for (int i = 0; i < arr.length; i++) {
-            int arrLength = arr[i].length;
-            for (int j = 0; j < arrLength; j++) {
-
+//            int arrLength = arr[i].length;
+            checkArraySize("Столбцы", arr[i].length);
+            for (int j = 0; j < arr[i].length; j++) {
                 try {
                     arrElement = Integer.parseInt(arr[i][j]);
                     arrSum += arrElement;
                 } catch (NumberFormatException e) {
                     String arrElementError = arr[i][j];
                     throw new MyArrayDataException(arrElementError, i, j);
-//                } finally {
-//                    System.out.println("Программа завершена");
                 }
             }
         }
         return arrSum;
     }
 
-    static void checkArraySize(String[][] arr) {
-        if (arr.length != 4) {
-            throw new MyArraySizeException(arr.length, ARRAY_SIZE);
+    static void checkArraySize(String str, int arrLength) {
+        if (arrLength != ARRAY_SIZE) {
+            throw new MyArraySizeException(str, arrLength, ARRAY_SIZE);
         }
     }
-
 
 }
--- Lesson02/MyArraySizeException.java
+++ Lesson02/MyArraySizeException.java
@@ -1,24 +1,14 @@
 package Lesson02;
 
-//public class MyArraySizeException {
+
 public class MyArraySizeException extends RuntimeException {
-//public class MyArraySizeException extends ArithmeticException {
-//public class MyArraySizeException extends NullPointerException {
-//public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
-//    public MyArraySizeException(String StrException) {
-//    public MyArraySizeException() {
-    public MyArraySizeException(int arrLength, int ARRAY_SIZE) {
-//        super(StrException);
-//        System.out.println("88888888888888888888888888888");
-//        System.out.println(StrException);
+
+    public MyArraySizeException(String str, int arrLength, int ARRAY_SIZE) {
         System.out.println("––– RuntimeException –––––––––––––––––––––––––––");
-        System.out.println("arrLength = " + arrLength);
-        System.out.println("массив не равен");
-        System.out.println("ARRAY_SIZE = " + ARRAY_SIZE);
+        System.out.println("Не верный размер массива");
+        System.out.println(str + " в массиве = " + arrLength);
+        System.out.println("Верный размер = " + ARRAY_SIZE);
         System.out.println("––––––––––––––––––––––––––––––");
-
     }
-
-
 
 }
