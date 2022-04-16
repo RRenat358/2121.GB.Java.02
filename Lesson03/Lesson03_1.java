b0198e18f1c4defc591eeff1e31f04d740202b46
[rrenat358] <[rrenat358@gmail.com]>
2022-04-16 21:00:35 +0300

    2100

---
+++ Lesson03/Lesson03_1.java
@@ -0,0 +1,69 @@
+package Lesson03;
+
+import java.util.*;
+/*
+1 Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и
+        вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
+        Посчитать, сколько раз встречается каждое слово.
+*/
+public class Lesson03_1 {
+
+    public static void main(String[] args) {
+        //======================================================================
+        System.out.println("––––––––––––––––––––––––––––––");
+        String[] arrayFruit = new String[]{
+                "Апельсин",
+                "Слива",
+                "Яблоко", "Яблоко", "Яблоко",
+                "Груша",
+                "Вишня",
+                "Киви",
+                "Малина", "Малина",
+                "Персик",
+                "Абрикос",
+                "Мандарин", "Мандарин", "Мандарин", "Мандарин"
+        };
+        System.out.println(arrayFruit.length + " = элементов всего");
+        System.out.println(Arrays.toString(arrayFruit));
+
+        //======================================================================
+        System.out.println("––––––––––––––––––––––––––––––");
+        Set<String> arrayFruitLinkedHashSet = new LinkedHashSet<>(); //уникальные
+        arrayFruitLinkedHashSet.addAll(Arrays.asList(arrayFruit));
+//        for (String arr : array01) {
+//            setArray01.add(arr);
+//        }
+        System.out.println(arrayFruitLinkedHashSet.size() + " = уникальных");
+        System.out.println(arrayFruitLinkedHashSet);
+
+        //======================================================================
+        System.out.println("––––––––––––––––––––––––––––––");
+        //наличие в массиве
+        String checkIsElement = "Укроп";
+        System.out.println("Элемент [ " + checkIsElement + " ] в массиве? == " +
+                arrayFruitLinkedHashSet.contains(checkIsElement));
+
+        //======================================================================
+        System.out.println("––––––––––––––––––––––––––––––");
+        // Количество каждого элемента в массиве
+        Map<String, Integer> mapCountIsElement = new TreeMap<>();
+        for (int i = 0; i < arrayFruit.length; i++) {
+            if (!mapCountIsElement.containsKey(arrayFruit[i])) {
+                mapCountIsElement.put(arrayFruit[i], 1);
+            } else {
+                mapCountIsElement.replace(arrayFruit[i],
+                        mapCountIsElement.get(arrayFruit[i]),
+                        mapCountIsElement.get(arrayFruit[i]) + 1
+                );
+            }
+        }
+//        treeMapCountIsElement.forEach((key, value) -> System.out.println(key + " " + value));
+        for (Map.Entry<String, Integer> entry : mapCountIsElement.entrySet()) {
+            System.out.println(entry.getKey() + "  =  " + entry.getValue());
+        }
+
+        //======================================================================
+        System.out.println("––––––––––––––––––––––––––––––");
+    }
+
+}
