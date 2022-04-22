f0da6d42e310cbf3e61e4110b0e186043d9b6e3e
rrenat358 <rrenat358@gmail.com>
2022-04-22 18:48:57 +0300

    1848

--- Lesson05/Lesson05.java
+++ Lesson05/Lesson05.java
@@ -44,9 +44,9 @@ public class Lesson05 {
     }
 
     public static void math2() throws InterruptedException {
-//        int SIZE = 10000000;
+        int SIZE = 10000000;
 //        int SIZE = 100000000;
-        int SIZE = 50;
+//        int SIZE = 20;
         int HALF_SIZE = SIZE / 2;
         float[] array2 = new float[SIZE];
 
@@ -60,60 +60,62 @@ public class Lesson05 {
         System.arraycopy(array2, HALF_SIZE, array2HalfRight, 0, HALF_SIZE);
 //        System.out.println(Arrays.toString(array2HalfLeft));
 //        System.out.println(Arrays.toString(array2HalfRight));
+
         Thread thread1 = new Thread(() -> {
-        Thread thread1a = new Thread(() -> {
-            for (int i = 0; i < array2HalfLeft.length; i++) {
-                array2HalfLeft[i] = (float) (array2HalfLeft[i] *
-                        Math.sin(0.2f + i / 5) *
-                        Math.cos(0.2f + i / 5) *
-                        Math.cos(0.4f + i / 2));
+
+            Thread thread1a = new Thread(() -> {
+                for (int i = 0; i < array2HalfLeft.length; i++) {
+                    array2HalfLeft[i] = (float) (array2HalfLeft[i] *
+                            Math.sin(0.2f + i / 5) *
+                            Math.cos(0.2f + i / 5) *
+                            Math.cos(0.4f + i / 2));
+                }
+            });
+
+            Thread thread1b = new Thread(() -> {
+                for (int j = 0; j < array2HalfRight.length; j++) {
+                    array2HalfRight[j] = (float) (array2HalfRight[j] *
+                            Math.sin(0.2f + j / 5) *
+                            Math.cos(0.2f + j / 5) *
+                            Math.cos(0.4f + j / 2));
+                }
+            });
+            thread1a.start();
+            thread1b.start();
+//            try {
+//                thread1a.join();
+//                thread1b.join();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+
+            while (thread1a.isAlive() || thread1b.isAlive()) {
+
             }
+
+
         });
-
-        Thread thread1b = new Thread(() -> {
-            for (int j = 0; j < array2HalfRight.length; j++) {
-                array2HalfRight[j] = (float) (array2HalfRight[j] *
-                        Math.sin(0.2f + j / 5) *
-                        Math.cos(0.2f + j / 5) *
-                        Math.cos(0.4f + j / 2));
-            }
-        });
-        });
-//        thread1.start();
-//        thread2.start();
-
-//        ThreadMatch threadMatch = new ThreadMatch();
-
-
-            thread1.start();
 
         thread1.start();
-
-
-
+        thread1.join();
 
         Thread thread2 = new Thread(() -> {
             System.arraycopy(array2HalfLeft, 0, array2, 0, HALF_SIZE);
             System.arraycopy(array2HalfRight, 0, array2, HALF_SIZE, HALF_SIZE);
-            System.out.println(Arrays.toString(array2HalfLeft));
-            System.out.println(Arrays.toString(array2HalfRight));
-            System.out.println(Arrays.toString(array2));
+//            System.out.println(Arrays.toString(array2HalfLeft));
+//            System.out.println(Arrays.toString(array2HalfRight));
+//            System.out.println(Arrays.toString(array2));
 
 
             System.out.println("math2 = " +
                     (System.currentTimeMillis() - timeStart) + "ms.");
         });
-        thread2.start();
-        thread2.join(1000);
 
-//
-//        Thread thread1 = new Thread(() -> {
-//            thread1a.start();
-//            thread1b.start();
-//        });
 //        thread1.start();
-//        thread2.start();
-//        thread2.join(1000);
+//        thread1.join();
+
+        thread2.start();
+        thread2.join();
 
 
     }
